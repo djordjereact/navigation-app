96b2cd8c4428af106457cb34959232d016016c8a
Dusan Mihailovic <duks05@yahoo.com>
2022-01-28 13:44:14 +0100

    First commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'navigation-app',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
---
+++ src/pages/Courses.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { IonHeader, IonContent, IonToolbar, IonTitle } from '@ionic/react';
+
+const Courses: React.FC = () => {
+    return (
+        <React.Fragment>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Courses</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                <h2>This works - courses page!</h2>
+            </IonContent>
+        </React.Fragment>
+    );
+};
+
+export default Courses;
