44dc18be7a85988c05ded9cf82920dbc9423ac74
Dusan Mihailovic <duks05@yahoo.com>
2022-01-29 12:58:10 +0100

    Made items swappable

--- src/pages/CourseGoals.tsx
+++ src/pages/CourseGoals.tsx
@@ -8,18 +8,18 @@ import {
     IonToolbar,
     IonBackButton,
     IonList,
-    IonItem, IonLabel, IonButton, IonIcon
+    IonItem, IonLabel, IonIcon, IonItemSliding, IonItemOptions, IonItemOption
 } from '@ionic/react';
 import { useParams } from 'react-router-dom'
 import {COURSE_DATA} from "./Courses";
-import {create} from "ionicons/icons";
+import {create, trash} from "ionicons/icons";
 
 const CourseGoals: React.FC = () => {
     const selectedCourseId = useParams<{courseId: string}>().courseId;
 
     const selectedCourse = COURSE_DATA.find(c => c.id === selectedCourseId);
 
-    const deleteItemHandler = () => {
+    const deleteGoalHandler = () => {
         console.log('Deleted')
     };
 
@@ -42,12 +42,24 @@ const CourseGoals: React.FC = () => {
                 {selectedCourse &&
                     <IonList>
                         {selectedCourse.goals.map(goal => (
-                            <IonItem key={goal.id} lines="full" button onClick={deleteItemHandler}>
-                                <IonLabel>{goal.text}</IonLabel>
-                                <IonButton fill="clear" color="dark" slot="end" onClick={startEditGoalHandler}>
-                                    <IonIcon slot="icon-only" icon={create} />
-                                </IonButton>
-                            </IonItem>
+                            <IonItemSliding key={goal.id}>
+                                <IonItemOptions side="start">
+                                    <IonItemOption onClick={deleteGoalHandler} color="danger">
+                                        <IonIcon slot="icon-only" icon={trash}/>
+                                    </IonItemOption>
+                                </IonItemOptions>
+                                <IonItem lines="full">
+                                    <IonLabel>{goal.text}</IonLabel>
+                                    {/*<IonButton fill="clear" color="dark" slot="end" onClick={startEditGoalHandler}>*/}
+                                    {/*    <IonIcon slot="icon-only" icon={create} />*/}
+                                    {/*</IonButton>*/}
+                                </IonItem>
+                                <IonItemOptions>
+                                    <IonItemOption onClick={startEditGoalHandler}>
+                                        <IonIcon slot="icon-only" icon={create}/>
+                                    </IonItemOption>
+                                </IonItemOptions>
+                            </IonItemSliding>
                         ))}
                     </IonList>
                 }
